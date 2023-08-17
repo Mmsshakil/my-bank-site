370939c7f917849c2f4d2c53d78e2f34453ce82f
programminghero <softhandshakil@gmail.com>
2023-08-17 16:11:46 +0600

    input section done with total value

--- js/deposite.js
+++ js/deposite.js
@@ -4,14 +4,33 @@ document.getElementById('button-deposite').addEventListener('click', function ()
     // console.log('helolo');
     // now take the value from deposite input 
     const depositeInput = document.getElementById('depositeInput');
-    const depositeValue = depositeInput.value;
+    const newDepositeValue = depositeInput.value;
     // console.log(depositeValue);
 
     // now take the value of total deposite by using inner text
     const defaultDeposit = document.getElementById('defaultDeposit');
-    const totalDeposite = defaultDeposit.innerText;
+    const previousTotalDeposite = defaultDeposit.innerText;
+
+    // make string to int and adding the previous value and the input value
+    const totalNewDeposite = parseInt(newDepositeValue) + parseInt(previousTotalDeposite);
 
     // change the value of total deposite
-    defaultDeposit.innerText = depositeValue;
+    defaultDeposit.innerText = totalNewDeposite;
+
+    // call the total money 
+    const totalBalanceDefault = document.getElementById('totalBalanceDefault');
+    // set the default value as innertext
+    const totalBalanceDefaultValue = totalBalanceDefault.innerText;
+    // console.log(parseFloat(totalBalanceDefaultValue) );
+
+    // update the total value 
+    // input value + default value
+    const totally = parseFloat(newDepositeValue) + parseFloat(totalBalanceDefaultValue);
+    totalBalanceDefault.innerText = totally;
+    // console.log(totally);
+
+
+    // clear the deposite input 
+    depositeInput.value = '';
 
 })
